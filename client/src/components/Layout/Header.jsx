8f241ca226611a006f0e41f959588bd0e14d3fcd
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-01-30 22:49:29 +0530

    First Commit

---
+++ client/src/components/Layout/Header.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { FaShop } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link to="/" className="navbar-brand">
+            <FaShop className="pb-1" /> OnMart
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className="nav-link"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/category" className="nav-link">
+                  Category
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/register" className="nav-link">
+                  Signup
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/cart" className="nav-link">
+                  Cart
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv, { config } from 'dotenv';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js'
+
+dotenv.config()
+//rest object
+const app = express();
+
+app.use(express.json());
+connectDB();
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+
+//PORT
+const PORT = process.env.PORT ;
+
+//run listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on port ${PORT}`);
+});
